a7484982ece9b19a4bf4a5c4573bc5120db5cf31
Babi Perina <bperinabezerra@gmail.com>
2022-09-16 15:53:50 -0700

    all home, without api connection

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,11 +6,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
       },
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(90deg, #9572FC 10.08%, #43E7AD 85.94%, #E1D55D 10.57%)',
+        'game-box-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
     },
   },
